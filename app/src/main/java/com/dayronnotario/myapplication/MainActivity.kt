04f9a5c722ece54086aa64e474d2e3225603792b
roberto-palmi <robpalmar@alu.edu.gva.es>
2025-10-19 07:58:21 +0200

    Cambiado idioma texto a castellano

--- app/src/main/java/com/dayronnotario/myapplication/MainActivity.kt
+++ app/src/main/java/com/dayronnotario/myapplication/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello ¡CALLABUZZTEMATARE!",
+        text = "Buenas tardes, ¡CALLABUZZTEMATARE!",
         modifier = modifier
     )
 }
