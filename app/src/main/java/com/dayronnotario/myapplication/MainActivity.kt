e62136207949f462f7bac627607b3db925863636
ezequielmenor <ezequielmenor@gmail.com>
2025-10-19 07:58:18 +0200

    Commit 1 Ezequiel

--- app/src/main/java/com/dayronnotario/myapplication/MainActivity.kt
+++ app/src/main/java/com/dayronnotario/myapplication/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello ¡CALLABUZZTEMATARE!",
+        text = "Hola ¡CALLABUZZTEMATARE!",
         modifier = modifier
     )
 }
